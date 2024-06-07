3f69d8a303a9265fc6153757fe6d7ff8cb9cc316
Yonko TSANEV <Yonko.TSANEV-EXT@akkodis.com>
2024-06-07 15:33:46 +0300

    Added outbound iptables rule for linux blocker.

--- blockerLinux.js
+++ blockerLinux.js
@@ -1,7 +1,7 @@
 // Function to block an IP using iptables
 function blockIPLinux (ip){ 
-  exec('sudo iptables -L INPUT -v -n', (listError, listStdout, listStderr) => {
-    if (listError) {
+  exec(`iptables -L -n | grep ${ip}`, (listError, listStdout, listStderr) => {
+    if (listError && !listError.message.includes("grep")) {
       console.error(`Error listing iptables rules: ${listError.message}`);
       return;
     }
@@ -15,17 +15,30 @@ function blockIPLinux (ip){
     if (ruleExists) {
       console.log(`IP ${ip} is already blocked.`);
     } else {
-      // Add the rule to block the IP if it doesn't already exist
-      exec(`sudo iptables -A INPUT -s ${ip} -j DROP`, (error, stdout, stderr) => {
+      // Add the rule to block inbound traffic from the IP if it doesn't already exist
+      exec(`iptables -A INPUT -s ${ip} -j DROP`, (error, stdout, stderr) => {
         if (error) {
-          console.error(`Error blocking IP ${ip}: ${error.message}`);
+          console.error(`Error blocking inbound IP ${ip}: ${error.message}`);
           return;
         }
         if (stderr) {
           console.error(`stderr: ${stderr}`);
           return;
         }
-        console.log(`Blocked IP: ${ip}`);
+        console.log(`Blocked Inbound IP: ${ip}`);
+      });
+
+      // Add the rule to block outbound traffic to the IP if it doesn't already exist
+      exec(`iptables -A OUTPUT -d ${ip} -j DROP`, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error blocking outbound IP ${ip}: ${error.message}`);
+          return;
+        }
+        if (stderr) {
+          console.error(`stderr: ${stderr}`);
+          return;
+        }
+        console.log(`Blocked Outbound IP: ${ip}`);
       });
     }
   });

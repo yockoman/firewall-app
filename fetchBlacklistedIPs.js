d51b5269ec43efa9f5710df3c753bcc289c8f375
Yonko TSANEV <Yonko.TSANEV-EXT@akkodis.com>
2024-06-08 11:59:03 +0300

    Added server that simulates the AbuseIPs API because it can only be requested 5 times a day and it is hard to test that way.

--- fetchBlacklistedIPs.js
+++ fetchBlacklistedIPs.js
@@ -4,34 +4,34 @@ const fs = require('fs');
 
 async function fetchBlacklistedIPs() {
  
-  // const API_KEY = 'e0769b72dcd4de0e37dc3714244c2991a534f54ed4bb5fd63ff0e5014c751507ef6bc1a0ecdfada2'; 
+   const API_KEY = 'e0769b72dcd4de0e37dc3714244c2991a534f54ed4bb5fd63ff0e5014c751507ef6bc1a0ecdfada2'; 
    try {
-  //   //After hitting this endpoint 5 times in a day it ret
-  //   //uncomment for getting the AbuseIPAPI blacklisted IP address list.urns status code 429 too many requests.
-  //   const response = await axios.get('https://api.abuseipdb.com/api/v2/blacklist', {
-  //     params: {
-  //       limit: '500000'
-  //     },
-  //     headers: {
-  //       'Accept': 'text/plain',
-  //       'Key':API_KEY
-  //     }
-  //   });
-
-  //   if (response.status !== 200) {
-  //     throw new Error('Network response was not ok ' + response.statusText);
-  //   }
-
-  //   var ipList = response.data.split('\n');
-    var ipList = ["203.159.92.150", "3.157.57.75","18.222.227.254","43.134.63.221","217.86.193.202","201.77.115.9","189.18.212.209","218.92.0.32","189.127.173.52","125.91.114.124", "43.155.174.208"];
-
-    const filePath = path.join(__dirname, 'blocked-ips.txt');
-    if (!fs.existsSync(filePath)) {
-      fs.writeFileSync(filePath, '');
-  }
-    fs.writeFileSync(filePath, ipList.join('\n'), 'utf8');
-    console.log(`Successfully wrote ${ipList.length} IPs to blocked-ips.txt`);
-
+    var ipList;
+    //After hitting this endpoint 5 times in a day it returns status code 429 too many requests.
+    //uncomment for getting the AbuseIPAPI blacklisted IP address list.
+    //await axios.get('https://api.abuseipdb.com/api/v2/blacklist', {
+    await axios.get('http://localhost:3000', {
+      params: {
+        limit: '500000'
+      },
+      headers: {
+        'Accept': 'text/plain',
+        'Key':API_KEY
+      }
+    })
+    .then((response) => {
+      if (response.status !== 200) {
+        throw new Error('Network response was not ok ' + response.statusText);
+      }
+      var ipList = response.data.split('\n');
+      const filePath = path.join(__dirname, 'blocked-ips.txt');
+      //create blocked-ips.txt file if it doesn't exist.
+      if (!fs.existsSync(filePath)) {
+        fs.writeFileSync(filePath, '');
+      }
+      fs.writeFileSync(filePath, ipList.join('\n'), 'utf8');
+      console.log(`Successfully wrote ${ipList.length} IPs to blocked-ips.txt`);
+    });
   } catch (error) {
     console.error('There has been a problem with your fetch operation:', error);
   }
--- index.js
+++ index.js
@@ -2,54 +2,50 @@ const { getOS } = require('./detectOS');
 const { blockIP: windowsBlockIP } = require('./blockerWindows');
 const { blockIPLinux: linuxBlockIP } = require('./blockerLinux');
 const { fetchBlacklistedIPs } = require('./fetchBlacklistedIPs');
+const fs = require('fs');
+const path = require('path');
 const interval = 24 * 60 * 60 * 1000; // 24 hours
 
 const blockIPs = async () => {
-    let os = getOS();
-    const fs = require('fs');
-    const path = require('path');
-
-    await fetchBlacklistedIPs(); 
-    //setInterval(fetchBlacklistedIPs, interval); //uncomment for this to be executed every 24 hours
-    const filePath = path.join(__dirname, 'blocked-ips.txt');
-    const data = fs.readFileSync(filePath, 'utf8');
-    const ipList = data.split('\n').filter(ip => ip.trim() !== '');
-    
-
-    // Block IPs based on OS
-    if (os === 'Windows') {
-        ipList.forEach(windowsBlockIP);
-    } else if (os === 'Linux') {
-        ipList.forEach(linuxBlockIP);
-    }
-
-    // Listen for changes in blocked-ips.txt
-    
-    const watchFile = 'blocked-ips.txt';
-    let debounceTimeout;
-    fs.watch(watchFile, (eventType, filename) => {
-        if (filename && eventType === 'change') {
-            clearTimeout(debounceTimeout);
-            debounceTimeout = setTimeout(() => {
-                fs.readFile(watchFile, 'utf8', (err, data) => {
-                    if (err) {
-                        console.error(`Error reading file: ${err}`);
-                        return;
-                    }
-                    const newIPs = data.split('\n').filter(ip => ip);
-                    newIPs.forEach(ip => {
-                        if (os === 'Windows') {
-                            windowsBlockIP(ip);
-                        } else if (os === 'Linux') {
-                            linuxBlockIP(ip);
-                        }
-                    });
-                });
-            }, 1000);
+    fetchBlacklistedIPs().then(() => {
+        const filePath = path.join(__dirname, 'blocked-ips.txt');
+        const data = fs.readFileSync(filePath, 'utf8');
+        const ipList = data.split('\n').filter(ip => ip.trim() !== '');
+        let os = getOS();
+         // Block IPs based on OS
+        if (os === 'Windows') {
+            ipList.forEach(windowsBlockIP);
+        } else if (os === 'Linux') {
+            ipList.forEach(linuxBlockIP);
         }
-    });
-
-    console.log(`${os} IP blocker is running...`);
+        
+        // Listen for changes in blocked-ips.txt
+        const watchFile = 'blocked-ips.txt';
+        let debounceTimeout;
+        fs.watch(watchFile, (eventType, filename) => {
+            if (filename && eventType === 'change') {
+                clearTimeout(debounceTimeout);
+                debounceTimeout = setTimeout(() => {
+                    fs.readFile(watchFile, 'utf8', (err, data) => {
+                        if (err) {
+                            console.error(`Error reading file: ${err}`);
+                            return;
+                        }
+                        const newIPs = data.split('\n').filter(ip => ip);
+                        newIPs.forEach(ip => {
+                            if (os === 'Windows') {
+                                windowsBlockIP(ip);
+                            } else if (os === 'Linux') {
+                                linuxBlockIP(ip);
+                            }
+                        });
+                    });
+                }, 1000);
+            }
+        });
+        console.log(`${os} IP blocker is running...`);
+    }); 
+    
 };
 
 blockIPs();
